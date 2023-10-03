ca4290d769e5b43866e83235105ebc425b7521fb
YinChenxx <pd9j59ae41o000@gmail.com>
2023-10-03 15:12:30 +0800

    1.上傳10/3練習

---
+++ ArrayPractice/Assets/Script/Array.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array : MonoBehaviour
+{
+    public GameObject[] gameObjects;
+    int gameobjectIndex;
+    public int index;
+    private void Awake()
+    {
+        for (gameobjectIndex = 0; gameobjectIndex <= 5; gameobjectIndex++)
+            gameObjects[gameobjectIndex].SetActive(false);
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            OpenGameObject(index);
+        if (Input.GetKey(KeyCode.S))
+            ActiveGameObject(index, true);
+    }
+    public void OpenGameObject(int index)
+    {
+        if (index >= gameObjects.Length)
+            return;
+        gameObjects[index].SetActive(true);
+    }
+    public void ActiveGameObject(int index,bool active=true)
+    {
+        if (active)
+            gameObjects[index].SetActive(true);
+        else
+            return;
+    }
+}
